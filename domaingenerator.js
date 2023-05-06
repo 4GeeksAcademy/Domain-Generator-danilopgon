760b5698b082ec1db77b5444a898f957ea8a84e2
danilopgon <113547781+danilopgon@users.noreply.github.com>
2023-05-06 08:23:42 +0000

    create domaingenerator.js

---
+++ domaingenerator.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+console.log("Hello, world!");
+
+const domainNameGenerator = () => {
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+};
